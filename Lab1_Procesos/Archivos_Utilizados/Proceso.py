d5bad578806460caef01419111f2d458604f509f
Guillermo_Zorrilla <undertale3544@gmail.com>
2025-06-20 14:40:03 -0300

    Primer commit - lab 1 completo

---
+++ Lab1_Procesos/Archivos_Utilizados/Proceso.py
@@ -0,0 +1,15 @@
+import time
+
+print("Estado: Nuevo (programa iniciado)")
+time.sleep(2)
+
+print("Estado: Listo (esperando CPU)")
+time.sleep(2)
+
+print("Estado: Ejecutando (en uso)")
+time.sleep(2)
+
+print("Estado: Bloqueado (esperando recurso)")
+time.sleep(2)
+
+print("Estado: Terminado")
